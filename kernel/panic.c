f90deb5b96853fb906a7ff2d737fad385d5f9bfa
bluestarsdv <turmadoyeahblu3@gmail.com>
2026-07-08 12:53:20 -0300

    Update panic.c

--- kernel/panic.c
+++ kernel/panic.c
@@ -1,26 +1,12 @@
 // kernel/panic.c
 #include <stdint.h>
 
-// Mudamos para a função que realmente existe no sistema principal
-extern void escrever_linha(const char *str, int linha, uint8_t cor);
-
 void kernel_panic(const char *mensagem_erro) {
-    // Código de cor para o modo texto (0x4F = Fundo Vermelho, Texto Branco)
-    uint8_t cor_erro = 0x4F;
+    (void)mensagem_erro; // Evita aviso de variável não utilizada no compilador
 
-    // Exibe a tela de Kernel Panic usando a função do sistema principal
-    escrever_linha("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", 5, cor_erro);
-    escrever_linha("              KERNEL PANIC OCORREU                ", 6, cor_erro);
-    escrever_linha("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", 7, cor_erro);
-    
-    escrever_linha("O Nai Kernel encontrou um erro critico.", 9, 0x0F);
-    escrever_linha("Motivo do erro:", 11, 0x0F);
-    escrever_linha(mensagem_erro, 12, cor_erro);
-    
-    escrever_linha("O sistema foi interrompido para protecao.", 14, 0x0F);
-
-    // Trava o processador em um loop infinito seguro
+    // Trava o processador em um loop infinito de segurança
     while (1) {
-        // Loop de espera
+        // No futuro, quando tivermos um driver de vídeo/framebuffer configurado,
+        // colocamos o código para pintar a tela de vermelho aqui.
     }
 }
